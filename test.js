eb3485f1bbf2ccb9c6fc73fd20a8aafd338e7b7b
thanhtungdev05 <166943839+thanhtungdev05@users.noreply.github.com>
2026-08-26 14:05:34 +0700

    Remove unused scripts and log files

--- test.js
+++
@@ -1,4 +0,0 @@
-﻿let html_str = 'var dhcpd_dns_all = "";';
-let val_dns = 'N/A,8.8.8.8,1.1.1.1';
-html_str = html_str.replace(/(var\s+dhcpd_dns_all\s*=\s*['"])[^'"]*?(['"])/i, '' + val_dns + '');
-console.log(html_str);
